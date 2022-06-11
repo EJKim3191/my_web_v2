18b50e9febd405d93e7de4bd8c11b1cffda3ebdf
EJKim <dmdwn3979@gmail.com>
2022-06-11 20:35:30 +0900

    Add: comment add

--- src/feature/MainReducer.js
+++ src/feature/MainReducer.js
@@ -3,11 +3,17 @@ import { createSlice } from "@reduxjs/toolkit";
 const main = createSlice({
   name: "MainReducer",
   initialState: {
+    // 현재 케릭터 위치
     x: 0,
     y: 0,
+    // 스크린 높낮이
     screenHeight: 0,
     screenWidth: 0,
+    // 계산된 움직임 값
     movementValue: 0,
+    movementValueByX: 0,
+    movementValueByY: 0,
+    // 처음 위치
     startPosX: 0,
     startPosY: 0,
   },
@@ -15,18 +21,22 @@ const main = createSlice({
     handleMove: (state, action) => {
       const beforeX = state.x;
       const beforeY = state.y;
+      //handle UP
       if (action.payload.keyCode === 38) {
         if (state.y < state.screenHeight / 2 - 2 * state.movementValue) {
           state.y = beforeY;
         } else state.y -= state.movementValue;
+      //handle DOWN
       } else if (action.payload.keyCode === 40) {
         if (state.y > state.screenHeight / 2 + 5 * state.movementValue)
           state.y = beforeY;
         else state.y += state.movementValue;
+      //handle LEFT
       } else if (action.payload.keyCode === 37) {
         if (state.x < state.screenWidth / 2 - 14 * state.movementValue)
           state.x = beforeX;
         else state.x -= state.movementValue;
+      //handle RIGHT
       } else if (action.payload.keyCode === 39) {
         if (state.x > state.screenWidth / 2 + 15 * state.movementValue)
           state.x = beforeX;
@@ -76,8 +86,9 @@ const main = createSlice({
       }
     },
     setScreen: (state) => {
-      state.screenHeight = window.innerHeight;
+      // state.screenHeight = window.innerHeight;
       state.screenWidth = window.innerWidth;
+      state.screenHeight = window.innerWidth*9/16;
     },
     calcStartPos: (state) => {
       state.x = window.innerWidth / 2;
