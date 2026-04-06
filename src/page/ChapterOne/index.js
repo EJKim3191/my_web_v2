14f2d246f91f25d6272d812b9b4bd7772d1f2904
EJKim <dmdwn3979@gmail.com>
2026-04-06 21:40:24 +0900

    add: timer for redirection

--- src/page/ChapterOne/index.js
+++ src/page/ChapterOne/index.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BackgroundContainer, Background } from "./styles";
 import webonconstruction from "../../img/pixelImg/background/webonconstruction.png"; //05->12
 import Loader from "../../components/Loader/Loader";
 
 export default function ChapterOne() {
+  const to = "https://team-scheduler-ai-omega.vercel.app/";
+  const delay = 5000;
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      window.location.href = to;
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <BackgroundContainer>
       <Background alt="background" src={webonconstruction} />
